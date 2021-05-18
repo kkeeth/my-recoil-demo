8f80eb63aa08265e99e8fe4c43c7531907f23a5e
kkeeth <zensin0082@gmail.com>
2021-05-18 16:17:17 +0900

    style: minor modifying

--- src/todo/TodoList.js
+++ src/todo/TodoList.js
@@ -4,6 +4,7 @@ import TodoItemCreator from './TodoItemCreator'
 import TodoListStats from './TodoListStats'
 import TodoListFilters from './TodoListFilters'
 import TodoItem from './TodoItem'
+import './Todo.css'
 
 const TodoList = () => {
   const todoList = useRecoilValue(todoListState)
